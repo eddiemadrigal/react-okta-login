964bbc874074f9ac784fe1d9884ef7ecaf55dc81
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-20 20:49:47 -0700

    app.js refactored

--- src/components/AppWithRouterAccess.js
+++ src/components/AppWithRouterAccess.js
@@ -14,18 +14,15 @@ const AppWithRouterAccess = () => {
 
  return (
   <Security
-   issuer="https://dev-966011.okta.com/oauth2/default"
-   clientId="0oacbqrpo3bgEkbLG4x6"
+   issuer=".okta.com/oauth2/default"
+   clientId=""
    redirectUri={window.location.origin + "/implicit/callback"}
    onAuthRequired={onAuthRequired}
    pkce={true}
   >
    <Route path="/" exact={true} component={Home} />
    <SecureRoute path="/dashboard" component={Dashboard} />
-   <Route
-    path="/login"
-    render={() => <Login issuer="https://dev-966011.okta.com/oauth2/default" />}
-   />
+   <Route path="/login" render={() => <Login issuer="/oauth2/default" />} />
    <Route path="/implicit/callback" component={LoginCallback} />
    <Route path="/logged_out" component={LoggedOut} />
   </Security>
